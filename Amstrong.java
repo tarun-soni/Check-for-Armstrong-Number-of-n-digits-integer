2dff7dc4ef1a033e8625dce9f0ffd096cc2b1059
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2017-10-31 19:12:35 +0530

    Add files via upload

---
+++ Amstrong.java
@@ -0,0 +1,52 @@
+import java.io.*;
+
+public class Amstrong 
+
+{
+	
+	public static void main (String[] args) 
+	
+	{
+        	 
+		 int no = 54748,  originalNo, count = 0;
+	
+        	  int n = no,   result=0;
+	
+        	  int remainder;
+	
+        	   while(n > 0)
+ 
+    	   	   {
+    
+			n = n/10;
+	  	     	count++;
+	
+        	   }
+
+        	   
+        	   originalNo = no;
+
+        		 while(originalNo!=0)
+
+	        	  {
+ 
+      	 		 remainder = originalNo % 10;
+
+	        		 result += Math.pow(remainder,count);
+
+        			 originalNo = originalNo/10;
+        	   
+		  }
+     
+   	   
+        	 if(result==no)
+        	      
+			  System.out.println(no+" is an Amstrong Number");
+        	    
+		else
+        	        
+			System.out.println(no+" is not an Amstrong Number");
+	
+	}
+
+}
