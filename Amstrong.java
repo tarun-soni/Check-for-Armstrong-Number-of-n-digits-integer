73e158b5c290c8586fac2e47109ee25a7b007d09
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2017-12-08 13:16:07 +0530

    Update Amstrong.java

--- Amstrong.java
+++ Amstrong.java
@@ -9,44 +9,23 @@ public class Amstrong
 	{
         	 
 		 int no = 54748,  originalNo, count = 0;
-	
         	  int n = no,   result=0;
-	
         	  int remainder;
-	
         	   while(n > 0)
- 
     	   	   {
-    
 			n = n/10;
 	  	     	count++;
-	
         	   }
-
-        	   
-        	   originalNo = no;
-
-        		 while(originalNo!=0)
-
-	        	  {
- 
-      	 		 remainder = originalNo % 10;
-
-	        		 result += Math.pow(remainder,count);
-
-        			 originalNo = originalNo/10;
-        	   
-		  }
-     
-   	   
-        	 if(result==no)
-        	      
+		originalNo = no;
+		while(originalNo!=0)
+		{
+			remainder = originalNo % 10;	result += Math.pow(remainder,count);
+			 originalNo = originalNo/10;
+		}
+        	if(result==no)
 			  System.out.println(no+" is an Amstrong Number");
-        	    
 		else
-        	        
 			System.out.println(no+" is not an Amstrong Number");
-	
 	}
 
 }
